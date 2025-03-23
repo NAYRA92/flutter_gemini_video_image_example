697a61da69157fe7e3cfa22807e5707794ea3d81
naira.hashem@gmail.com <naira.hashem@gmail.com>
2025-03-24 02:00:59 +0300

    first commit

---
+++ lib/providers/gemini_provider.dart
@@ -0,0 +1,62 @@
+import 'dart:typed_data';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_gemini/services/gemini_service.dart';
+import 'package:google_generative_ai/google_generative_ai.dart';
+
+class GeminiProvider extends ChangeNotifier {
+  static GenerativeModel _initModel() {
+    // final key = dotenv.env['GEMINI_API_KEY'];
+    final key = "AIzaSyCJJDVNUWUoX53GXIKbKYD7Jam6BwqBrWo";
+
+    if (key!.isEmpty) {
+      throw Exception('GEMINI_API_KEY not found');
+    }
+    return GenerativeModel(model: 'gemini-2.0-flash', apiKey: key);
+  }
+
+  static final _geminiService = GeminiService(model: _initModel());
+
+  String? response;
+  bool isLoading = false;
+
+  Future<void> generateContentFromText({
+    required String prompt,
+  }) async {
+    isLoading = true;
+    notifyListeners();
+    response = null;
+    response = await _geminiService.generateContentFromText(prompt: prompt);
+    isLoading = false;
+    notifyListeners();
+  }
+
+  Future<void> generateContentFromImage({
+    required String prompt,
+    required Uint8List bytes,
+  }) async {
+    isLoading = true;
+    notifyListeners();
+    response = null;
+    final dataPart = DataPart(
+      'video/',
+      // 'image/jpeg',
+      bytes,
+    );
+
+    response = await _geminiService.generateContentFromImage(
+      prompt: prompt,
+      dataPart: dataPart,
+    );
+
+    isLoading = false;
+    notifyListeners();
+  }
+
+  void reset() {
+    response = null;
+    isLoading = false;
+    notifyListeners();
+  }
+}
